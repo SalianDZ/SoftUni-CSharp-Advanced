86ceb40d5ac3528b8c7597af064b340692dc99fe
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-16 22:07:44 +0300

    Stacks and Queues - Exercise

---
+++ Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -0,0 +1,63 @@
+﻿Dictionary<int, Pump> pumps = new();
+int pumpsCount = int.Parse(Console.ReadLine());
+int truck = 0;
+
+for (int i = 0; i < pumpsCount; i++)
+{
+    int[] pumpsInfo = Console.ReadLine()
+        .Split().Select(int.Parse).ToArray();
+
+    pumps[i] = new(pumpsInfo[0], pumpsInfo[1]);
+}
+
+int counter = 0;
+for (int i = 0; i < pumpsCount; i++)
+{
+    bool notEnough = false;
+    int iCopy = i;
+    counter = 0;
+    while (true)
+    {
+        if (truck < 0)
+        {
+            break;
+        }
+        truck += pumps[iCopy].PetrolAmount;
+        truck -= pumps[iCopy].Distance;
+        if (truck < 0)
+        {
+            truck = 0;
+            notEnough = true;
+            break;
+        }
+        iCopy++;
+        counter++;
+        if (counter > pumpsCount)
+        {
+            Console.WriteLine(i);
+            return;
+        }
+        if (iCopy > pumpsCount - 1)
+        {
+            iCopy = 0;
+        }
+
+    }
+    if (notEnough)
+    {
+        continue;
+    }
+}
+
+class Pump
+{
+    public Pump(int petrolAmount, int distance)
+    {
+        PetrolAmount = petrolAmount;
+        Distance = distance;
+    }
+
+    public int PetrolAmount { get; set; }
+
+    public int Distance { get; set; }
+}
---
+++ Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,65 @@
+﻿string input = Console.ReadLine();
+Stack<char> stack = new Stack<char>(input);
+int begginingCount = stack.Count;
+Stack<char> reversedStack = new Stack<char>(stack);
+bool isSame = true;
+
+if (input.Length % 2 != 0)
+{
+	Console.WriteLine("NO");
+	return;
+}
+
+while (stack.Count > 0 && reversedStack.Count > 0)
+{
+    if (stack.Count <= begginingCount || reversedStack.Count <= begginingCount)
+    {
+        break;
+    }
+	if (reversedStack.Peek() == '{')
+	{
+		if (stack.Peek() == '}')
+		{
+			stack.Pop();
+			reversedStack.Pop();
+			continue;
+		}
+		isSame = false;
+		break;
+	}
+	else if (reversedStack.Peek() == '[')
+	{
+        if (stack.Peek() == ']')
+        {
+            stack.Pop();
+            reversedStack.Pop();
+            continue;
+        }
+        isSame = false;
+        break;
+    }
+	else if (reversedStack.Peek() == '(')
+	{
+        if (stack.Peek() == ')')
+        {
+            stack.Pop();
+            reversedStack.Pop();
+            continue;
+        }
+        isSame = false;
+        break;
+    }
+    else
+    {
+        isSame = false;
+        break;
+    }
+}
+if (isSame)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
