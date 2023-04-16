dc1f67955eff7a70915a180e4299a4fae95284de
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-16 16:08:28 +0300

    Stacks and Queues - Exercises

---
+++ Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security;
+int[] box = Console.ReadLine()
+    .Split().Select(int.Parse).ToArray();
+int capacity = int.Parse(Console.ReadLine());
+int currentCapacity = capacity;
+Stack<int> clothes = new(box);
+int containerCounter = 1;
+
+while (clothes.Count > 0)
+{
+	currentCapacity -= clothes.Peek();
+
+	if (currentCapacity < 0)
+	{
+		currentCapacity = capacity;
+		containerCounter++;
+		continue;
+	}
+    clothes.Pop();
+}
+Console.WriteLine(containerCounter);
---
+++ Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security;
+
+string[] input = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+Queue<string> songs = new(input);
+
+while (songs.Count != 0)
+{
+    string currentCommand = Console.ReadLine();
+    string[] command = currentCommand.Split(" ");
+    
+
+    if (command[0] == "Play")
+    {
+        if (songs.Count <= 0)
+        {
+            continue;
+        }
+        songs.Dequeue();
+    }
+    else if (command[0] == "Add")
+    {
+        string currentSong = string.Empty;
+
+        for (int i = 4; i < currentCommand.Length; i++)
+        {
+            currentSong += currentCommand[i];
+        }
+
+        if (songs.Contains(currentSong))
+        {
+            Console.WriteLine($"{currentSong} is already contained!");
+            continue;
+        }
+        songs.Enqueue(currentSong);
+    }
+    else if (command[0] == "Show")
+    {
+        Console.WriteLine(String.Join(", ", songs));
+    }
+}
+
+Console.WriteLine("No more songs!");
