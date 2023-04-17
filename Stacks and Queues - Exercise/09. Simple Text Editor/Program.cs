cf3379328fead0444b8c049d88458e1a2c780f4c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-17 22:32:04 +0300

    Stacks and Queues - Exercise

---
+++ Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,33 @@
+﻿int commandsCount = int.Parse(Console.ReadLine());
+Stack<string> changes = new();
+string text = string.Empty;
+
+
+for (int i = 0; i < commandsCount; i++)
+{
+    string[] tokens = Console.ReadLine()
+        .Split();
+
+    int command = int.Parse(tokens[0]);
+
+    switch (command)
+    {
+        case 1:
+            changes.Push(text);
+            string substring = tokens[1];
+            text += substring;
+            break;
+        case 2:
+            changes.Push(text);
+            int count = int.Parse(tokens[1]);
+            text = text.Remove(text.Length - count);
+            break;
+        case 3:
+            int index = int.Parse(tokens[1]);
+            Console.WriteLine(text[index - 1]);
+            break;
+        case 4:
+            text = changes.Pop();
+            break;
+    }
+}
---
+++ Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Security;
+
+int greenLightDuratrion = int.Parse(Console.ReadLine());
+int freeWindowDuration = int.Parse(Console.ReadLine());
+Queue<string> cars = new();
+int counter = 0;
+
+
+while (true)
+{
+    int currentGreenLightDuration = greenLightDuratrion;
+    string tokens = Console.ReadLine();
+
+	if (tokens == "END")
+	{
+		break;
+	}
+	else if (tokens == "green")
+	{
+		if (cars.Count <= 0)
+		{
+			continue;
+		}
+		while (currentGreenLightDuration > 0)
+		{
+			if (cars.Count <= 0)
+			{
+				break;
+			}
+            string currentCar = cars.Peek();
+
+            if (currentCar.Length <= currentGreenLightDuration)
+            {
+                cars.Dequeue();
+				currentGreenLightDuration -= currentCar.Length;
+				counter++;
+				continue;
+            }
+			else
+			{
+				//This code can cause problems
+				int currentCarSeconds = currentCar.Length - currentGreenLightDuration;
+
+				if (currentCarSeconds > freeWindowDuration)
+				{
+					int characterHit = currentCar.Length - (currentCarSeconds - freeWindowDuration);
+					if (characterHit >= currentCar.Length || characterHit < 0)
+					{
+						continue;
+					}
+					Console.WriteLine("A crash happened!");
+					Console.WriteLine($"{currentCar} was hit at {currentCar[characterHit]}.");
+					return;
+				}
+				else
+				{
+					counter++;
+					cars.Dequeue();
+					break;
+				}
+			}
+        }
+		continue;
+	}
+
+	cars.Enqueue(tokens);
+}
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{counter} total cars passed the crossroads.");
