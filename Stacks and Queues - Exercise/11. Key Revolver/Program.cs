457e4f48e810e7708cec92d2a411471f41e65f95
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-18 01:08:27 +0300

    Stacks and Queues - Exercise

---
+++ Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,63 @@
+﻿int bulletPrice = int.Parse(Console.ReadLine());
+int gunBarrelSize = int.Parse(Console.ReadLine());
+Stack<int> bullets = new(
+    Console.ReadLine()
+    .Split().Select(int.Parse)
+    );
+Queue<int> locks = new(
+        Console.ReadLine()
+        .Split().Select(int.Parse)
+    );
+int intelligenceValue = int.Parse(Console.ReadLine());
+int bulletCounter = 0;
+
+while (bullets.Count > 0)
+{
+    for (int i = 1; i <= gunBarrelSize; i++)
+    {
+        if (bullets.Count <= 0)
+        {
+            break;
+        }
+        if (locks.Count <= 0)
+        {
+            int bulletMoney = bulletCounter * bulletPrice;
+            int moneyEarned = intelligenceValue - bulletMoney;
+            Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+            return;
+        }
+        else
+        {
+            int currentBulletPower = bullets.Peek();
+            int currentLockPower = locks.Peek();
+            if (currentBulletPower <= currentLockPower)
+            {
+                Console.WriteLine("Bang!");
+                bullets.Pop();
+                locks.Dequeue();
+                bulletCounter++;
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Ping!");
+                bullets.Pop();
+                bulletCounter++;
+            }
+        }
+    }
+    if (bullets.Count > 0)
+    {
+        Console.WriteLine("Reloading!");
+    }
+}
+if (locks.Count > 0)
+{
+    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+}
+else if (locks.Count <= 0 && bullets.Count >= 0)
+{
+    int bulletMoney = bulletCounter * bulletPrice;
+    int moneyEarned = intelligenceValue - bulletMoney;
+    Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+}
---
+++ Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,53 @@
+﻿Queue<int> cups = new(
+    Console.ReadLine()
+    .Split().Select(int.Parse)
+    );
+Stack<int> waterBottles = new(
+    Console.ReadLine()
+    .Split().Select(int.Parse)
+    );
+int wastedWater = 0;
+
+while (waterBottles.Count > 0)
+{
+    if (cups.Count <= 0)
+    {
+        break;
+    }
+    int currentBottle = waterBottles.Peek();
+    int currentCup = cups.Peek();
+
+    if (currentBottle >= currentCup)
+    {
+        wastedWater += currentBottle - currentCup;
+        cups.Dequeue();
+        waterBottles.Pop();
+    }
+    else if (currentBottle < currentCup)
+    {
+        int remainder = currentCup - waterBottles.Pop();
+
+        while (remainder > 0)
+        {
+            if (waterBottles.Peek() >= remainder)
+            {
+                wastedWater += waterBottles.Pop() - remainder;
+                cups.Dequeue();
+                remainder = 0;
+                continue;
+            }
+
+            remainder -= waterBottles.Pop();
+        }
+    }
+}
+if (cups.Count > 0)
+{
+    Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+    Console.WriteLine($"Wasted litters of water: {wastedWater}");
+}
+else
+{
+    Console.WriteLine($"Bottles: {string.Join(" ", waterBottles)}");
+    Console.WriteLine($"Wasted litters of water: {wastedWater}");
+}
