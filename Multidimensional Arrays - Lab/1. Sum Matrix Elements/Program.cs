d500c49e8aad4321eb346a94e50d9eb926a09075
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-19 02:37:32 +0300

    Multidimensional Arrays - Lab

---
+++ Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] matrixInfo = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+int rows = matrixInfo[0];
+int cols = matrixInfo[1];
+int[,] matrix = new int[rows, cols];
+int sum = 0;
+
+for (int row = 0; row < rows; row++)
+{
+	int[] data = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+	for (int col = 0; col < cols; col++)
+	{
+		matrix[row, col] = data[col];
+		sum += matrix[row, col];
+    }
+}
+Console.WriteLine(rows);
+Console.WriteLine(cols);
+Console.WriteLine(sum);
