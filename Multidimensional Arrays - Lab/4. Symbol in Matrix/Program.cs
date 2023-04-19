e981d89da8c954c5a7e287485eea2c81c452a775
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-19 03:32:22 +0300

    Multidimensional Arrays - Lab

---
+++ Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,31 @@
+﻿int matrixSize = int.Parse(Console.ReadLine());
+char[,] matrix = new char[matrixSize, matrixSize];
+bool isFound = false;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    string currentInput = Console.ReadLine();
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = currentInput[col];
+    }
+}
+char searchedSymbol = char.Parse(Console.ReadLine());
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        if (matrix[row, col] == searchedSymbol)
+        {
+            Console.WriteLine($"({row}, {col})");
+            isFound = true;
+            return;
+        }
+    }
+}
+
+if (!isFound)
+{
+    Console.WriteLine($"{searchedSymbol} does not occur in the matrix");
+}
