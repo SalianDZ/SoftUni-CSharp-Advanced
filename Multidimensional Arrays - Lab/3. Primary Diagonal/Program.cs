9a33a41e72e6d62871342c474f15f976b9d668dc
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-19 03:08:11 +0300

    Multidimensional Arrays - Lab

---
+++ Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,22 @@
+﻿int matrixSize = int.Parse(Console.ReadLine());
+int[,] matrix = new int[matrixSize, matrixSize];
+int sum = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = data[col];
+    }
+}
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = row; col < matrix.GetLength(1); col++)
+    {
+        sum += matrix[row, col];
+        break;
+    }
+}
+Console.WriteLine(sum);
