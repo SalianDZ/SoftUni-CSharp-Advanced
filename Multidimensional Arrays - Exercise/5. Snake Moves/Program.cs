105404a3b13afe2ede25b581c807a021cc322dd6
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-23 22:54:30 +0300

    Snake Moves

---
+++ Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security;
+
+int[] matrixInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int rows = matrixInfo[0];
+int cols = matrixInfo[1];
+char[,] matrix = new char[rows, cols];
+
+int turn = 1;
+
+string input = Console.ReadLine();
+Queue<char> text = new(input);
+int colStart = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    if (row % 2 == 0)
+    {
+        for (int col = colStart; col < matrix.GetLength(1); col++)
+        {
+            char currentSymbol = text.Dequeue();
+            matrix[row, col] = currentSymbol;
+            text.Enqueue(currentSymbol);
+        }
+    }
+    else
+    {
+        for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+        {
+            char currentSymbol = text.Dequeue();
+            matrix[row, col] = currentSymbol;
+            text.Enqueue(currentSymbol);
+        }
+    }
+
+    
+}
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        Console.Write(matrix[row, col]);
+    }
+    Console.WriteLine();
+}
