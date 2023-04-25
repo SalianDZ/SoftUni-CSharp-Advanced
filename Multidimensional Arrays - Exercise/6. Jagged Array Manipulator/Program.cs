036a3e0bdf9014fd41d3eb55aa1c9a45ae080579
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-04-25 23:44:15 +0300

    Multidimensional Arrays - Exercise

---
+++ Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,79 @@
+﻿int rows = int.Parse(Console.ReadLine());
+int[][] jaggedArray = new int[rows][];
+
+for (int row = 0; row < rows; row++)
+{
+    jaggedArray[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+}
+
+for (int row = 0; row < rows; row++)
+{
+	if (row + 1 >= jaggedArray.Length)
+	{
+		break;
+	}
+
+	if (jaggedArray[row].Length == jaggedArray[row + 1].Length)
+	{
+		for (int col = 0; col < jaggedArray[row].Length; col++)
+		{
+			jaggedArray[row][col] *= 2;
+        }
+        for (int col = 0; col < jaggedArray[row + 1].Length; col++)
+        {
+            jaggedArray[row + 1][col] *= 2;
+        }
+    }
+    else
+    {
+        for (int col = 0; col < jaggedArray[row].Length; col++)
+        {
+            jaggedArray[row][col] /= 2;
+        }
+        for (int col = 0; col < jaggedArray[row + 1].Length; col++)
+        {
+            jaggedArray[row + 1][col] /= 2;
+        }
+    }
+}
+
+while (true)
+{
+    string[] command = Console.ReadLine().Split();
+
+    if (command[0] == "End")
+    {
+        break;
+    }
+    else if (command[0] == "Add")
+    {
+        int row = int.Parse(command[1]);
+        int col = int.Parse(command[2]);
+        int value =int.Parse(command[3]);
+
+        if (row >= 0 && row < jaggedArray.Length && col >= 0 && col < jaggedArray[row].Length)
+        {
+            jaggedArray[row][col] += value;
+        }
+    }
+    else if (command[0] == "Subtract")
+    {
+        int row = int.Parse(command[1]);
+        int col = int.Parse(command[2]);
+        int value = int.Parse(command[3]);
+
+        if (row >= 0 && row < jaggedArray.Length && col >= 0 && col < jaggedArray[row].Length)
+        {
+            jaggedArray[row][col] -= value;
+        }
+    }
+}
+
+for (int row = 0; row < jaggedArray.Length; row++)
+{
+    for (int col = 0; col < jaggedArray[row].Length; col++)
+    {
+        Console.Write(jaggedArray[row][col] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Multidimensional Arrays - Exercise/7. Knight Game/Program.cs
@@ -0,0 +1,11 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+string[,] matrix = new string[size, size];
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    string[] data = Console.ReadLine().Split("");
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = data[col];
+    }
+}
