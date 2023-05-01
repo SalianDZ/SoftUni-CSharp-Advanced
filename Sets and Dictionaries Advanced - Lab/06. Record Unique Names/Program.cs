f3a52790412904d3956c386d03fed60e2732527e
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-01 23:37:08 +0300

    Sets and Dictionaries Advanced - Lab

---
+++ Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,12 @@
+﻿int count = int.Parse(Console.ReadLine());
+HashSet<string> set = new HashSet<string>();
+
+for (int i = 0; i < count; i++)
+{
+    set.Add(Console.ReadLine());
+}
+
+foreach (var name in set)
+{
+    Console.WriteLine(name);
+}
---
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,37 @@
+﻿HashSet<string> parkingLot = new();
+
+while (true)
+{
+    string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+	if (input[0] == "END")
+	{
+		break;
+	}
+	else if (input[0] == "IN")
+	{
+		string number = input[1];
+		parkingLot.Add(number);
+	}
+	else if (input[0] == "OUT")
+	{
+        string number = input[1];
+
+		if (parkingLot.Contains(number))
+		{
+			parkingLot.Remove(number);
+		}
+    }
+}
+
+if (parkingLot.Count > 0)
+{
+    foreach (var number in parkingLot)
+    {
+		Console.WriteLine(number);
+    }
+}
+else
+{
+	Console.WriteLine("Parking Lot is Empty");
+}
