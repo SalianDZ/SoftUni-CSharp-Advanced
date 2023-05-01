676766520117b57eb781ffb1754fd2e45eee9beb
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-01 23:37:26 +0300

    Sets and Dictionaries Advanced - Lab

---
+++ Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿Dictionary<string, Dictionary<string, double>> shops = new();
+
+while (true)
+{
+    string[] data = Console.ReadLine().Split(", ");
+
+	if (data[0] == "Revision")
+	{
+		break;
+	}
+	
+	string shop = data[0];
+	string product = data[1];
+	double price = double.Parse(data[2]);
+
+	if (!shops.ContainsKey(shop))
+	{
+		shops.Add(shop, new Dictionary<string, double>());
+	}
+	else
+	{
+		shops[shop].Add(product, price);
+	}
+
+	if (!shops[shop].ContainsKey(product))
+	{
+		shops[shop].Add(product, price);
+	}
+	else
+	{
+		shops[shop][product] = price;
+	}
+}
+
+shops = shops.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+foreach (var shop in shops)
+{
+	Console.WriteLine($"{shop.Key}-> ");
+
+	foreach (var product in shop.Value)
+	{
+		Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+	}
+}
---
+++ Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,31 @@
+﻿Dictionary<string, Dictionary<string, List<string>>> continents = new();
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string[] data = Console.ReadLine().Split();
+    string continent = data[0];
+    string country = data[1];
+    string city = data[2];
+
+    if (!continents.ContainsKey(continent))
+    {
+        continents.Add(continent, new Dictionary<string, List<string>>());
+    }
+
+    if (!continents[continent].ContainsKey(country))
+    {
+        continents[continent].Add(country, new List<string>());
+    }
+
+    continents[continent][country].Add(city);
+}
+
+foreach (var continent in continents)
+{
+    Console.WriteLine($"{continent.Key}:");
+    foreach (var country in continent.Value)
+    {
+        Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+    }
+}
