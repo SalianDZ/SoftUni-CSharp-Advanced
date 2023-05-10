0dccbf59179c105879863640779282d37159a534
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-11 00:13:00 +0300

    04. Even Times

---
+++ Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = int.Parse(Console.ReadLine());
+Dictionary<int, int> appearances = new Dictionary<int, int>();
+
+for (int i = 0; i < count; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+
+	if (!appearances.ContainsKey(currentNumber))
+	{
+		appearances[currentNumber] = 0;
+	}
+
+	appearances[currentNumber]++;
+}
+
+Console.WriteLine(appearances.Single(x => x.Value % 2 == 0).Key);
