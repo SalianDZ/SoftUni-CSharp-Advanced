0c085393b452632f00ea5fe7d8cf1c25d23587b4
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-11 00:16:13 +0300

    05. Count Symbols

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,17 @@
+﻿SortedDictionary<char, int> appearances = new();
+string text = Console.ReadLine();
+
+foreach (char ch in text)
+{
+	if (!appearances.ContainsKey(ch))
+	{
+		appearances[ch] = 0;
+	}
+
+	appearances[ch]++;
+}
+
+foreach (var appearance in appearances)
+{
+	Console.WriteLine($"{appearance.Key}: {appearance.Value} time/s");
+}
