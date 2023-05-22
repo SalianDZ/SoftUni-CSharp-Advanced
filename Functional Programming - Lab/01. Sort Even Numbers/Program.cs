ad61e94ae144d92439e81926fe5cb05090637a0c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-22 15:49:42 +0300

    Functional Programming - Lab

---
+++ Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,3 @@
+﻿int[] numbers = Console.ReadLine().Split(", ")
+	.Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+Console.WriteLine(String.Join(", ", numbers));
---
+++ Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = Console.ReadLine().Split(", ").Select(Parse).ToArray();
+Console.WriteLine(numbers.Length);
+Console.WriteLine(numbers.Sum());
+
+int Parse(string x)
+{
+    int convertedInt = int.Parse(x);
+    return convertedInt;
+}
