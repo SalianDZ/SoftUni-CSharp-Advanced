9bdf660ae4f3a144c4537a61b878e3b5e6770d19
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-22 16:12:08 +0300

    Functional Programming - Lab

---
+++ Functional Programming - Lab/03.Count Uppercase Words/Program.cs
@@ -0,0 +1,16 @@
+﻿Func<string, bool> filter = GetUppercaseWords;
+
+List<string> text = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+	.Select(s => s)
+    .Where(GetUppercaseWords).ToList();
+
+bool GetUppercaseWords (string input)
+{
+	if (char.IsUpper(input[0]))
+	{
+		return true;
+	}
+	return false;
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, text));
---
+++ Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,7 @@
+﻿double[] prices = Console.ReadLine().Split(", ")
+    .Select(double.Parse).Select(d => d * 1.2).ToArray();
+
+foreach (var price in prices)
+{
+    Console.WriteLine($"{price:f2}");
+}
