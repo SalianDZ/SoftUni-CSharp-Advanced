d55a40fb7191b82cf74571314fdb2e4a3b38a156
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-22 17:03:28 +0300

    Functional Programming - Lab

---
+++ Functional Programming - Lab/05. Filter By Age/Program.cs
@@ -0,0 +1,72 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<Person> people = new();
+
+for (int i = 0; i < n; i++)
+{
+            string[] tokens = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string currentName = tokens[0];
+            int currentAge = int.Parse(tokens[1]);
+
+            Person person = new Person(currentName, currentAge);
+            people.Add(person);
+}
+
+string condition = Console.ReadLine();
+int ageThreshold = int.Parse(Console.ReadLine());
+string formatType = Console.ReadLine();
+
+Func<Person,int, bool> filter = CreateFilter(condition);
+
+people = people.Where(p => filter(p, ageThreshold)).ToList();
+
+Action<Person> formatter = GetFormatter(formatType);
+
+foreach (var person in people)
+ {
+            formatter(person);
+ }
+
+Func<Person, int, bool> CreateFilter(string condition)
+{
+    if (condition == "younger")
+    {
+        return (p, value) => p.Age < value;
+    }
+    else if (condition == "older")
+    {
+        return (Person p, int value) => p.Age >= value;
+    }
+    return null;
+}
+
+
+Action<Person> GetFormatter(string formatType)
+    {
+        if (formatType == "name age")
+        {
+            return p => Console.WriteLine($"{p.Name} - {p.Age}");
+        }
+        if (formatType == "name")
+        {
+            return p => Console.WriteLine($"{p.Name}");
+        }
+        if (formatType == "age")
+        {
+            return p => Console.WriteLine($"{p.Age}");
+        }
+        return null;
+    }
+
+
+public class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
