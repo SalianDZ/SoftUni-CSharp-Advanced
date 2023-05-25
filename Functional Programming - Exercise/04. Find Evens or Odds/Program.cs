ba7ee1547c12838bbdc3fbc93b3b2a274d9cfc7d
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-25 20:14:20 +0300

    Functional Programming - Exercise

---
+++ Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] startAndEnd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int start = startAndEnd[0];
+int end = startAndEnd[1];
+List<int> range = Enumerable.Range(start, end - start + 1).ToList();
+List<int> result = new List<int>();
+string command = Console.ReadLine();
+
+Predicate<int> filter = number => number % 2 == 0;
+
+for (int i = 0; i < range.Count; i++)
+{
+	if (command == "even")
+	{
+		if (filter(range[i]))
+		{
+			result.Add(range[i]);
+		}
+	}
+	else
+	{
+		if (command == "odd")
+		{
+            if (!filter(range[i]))
+            {
+                result.Add(range[i]);
+            }
+        }
+	}
+}
+
+Console.WriteLine(string.Join(" ", result));
---
+++ Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,56 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+Func<List<int>, List<int>> addFunction = numbers =>
+{
+	for (int i = 0; i < numbers.Count; i++)
+	{
+		numbers[i] = numbers[i] + 1;
+	}
+	return numbers;
+};
+
+Func<List<int>, List<int>> subtractFunction = numbers =>
+{
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        numbers[i] = numbers[i] - 1;
+    }
+    return numbers;
+};
+
+Func<List<int>, List<int>> multiplyFunction = numbers =>
+{
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        numbers[i] = numbers[i] * 2;
+    }
+    return numbers;
+};
+
+Action<List<int>> printNumbers = numbers => Console.WriteLine(string.Join(" ", numbers));
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+	if (command == "end")
+	{
+		break;
+	}
+	else if (command == "add")
+	{
+		addFunction(numbers); 
+	}
+	else if (command == "multiply")
+	{
+		multiplyFunction(numbers);
+	}
+	else if (command == "subtract")
+	{
+		subtractFunction(numbers);
+	}
+	else if (command == "print")
+	{
+		printNumbers(numbers);
+	}
+
+}
