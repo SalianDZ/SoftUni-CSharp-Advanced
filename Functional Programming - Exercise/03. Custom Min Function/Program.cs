140c43894569ddb9427836b34090c167327e6d10
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-25 18:50:15 +0300

    03. Custom Min Function

---
+++ Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,17 @@
+﻿Func<HashSet<int>, int> returnTheSmallestNumber = numbers =>
+{
+    int minValue = int.MaxValue;
+
+	foreach (var number in numbers)
+	{
+		if (number < minValue)
+		{
+			minValue = number;
+		}
+	}
+	return minValue;
+};
+
+HashSet<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+
+Console.WriteLine(returnTheSmallestNumber(numbers));
