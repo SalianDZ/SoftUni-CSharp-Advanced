c7ad2edf87c5f9ac1a09d2923b4ff0d8e5ca418c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-25 18:38:56 +0300

    Functional Programming - Exercise

---
+++ Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,3 @@
+﻿Action<string[]> printStrings = strings => Console.WriteLine(string.Join(Environment.NewLine, strings));
+string[] strings = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+printStrings(strings);
---
+++ Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,8 @@
+﻿Action<string[]> printStrings = strings => {
+     foreach (var currentString in strings)
+     {
+         Console.WriteLine($"Sir {currentString}");
+     }
+ };
+string[] strings = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+printStrings(strings);
