00572d5f36bafc05e5dc974850d6ae466bb7b007
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-27 17:18:22 +0300

    06. Reverse And Exclude

---
+++ Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,35 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+int divider = int.Parse(Console.ReadLine());
+
+Func<List<int>, Predicate<int>, List<int>> filter = (numbers, match) =>
+{
+    List<int> result = new();
+
+    foreach (var number in numbers)
+    {
+        if (!match(number))
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+};
+
+Func<List<int>, List<int>> reverseNumbers = numbers =>
+{
+    List<int> result = new();
+
+
+    for (int i = numbers.Count - 1; i >= 0; i--)
+    {
+        result.Add(numbers[i]);
+    }
+
+    return result;
+};
+
+numbers = filter(numbers, n => n % divider == 0);
+numbers = reverseNumbers(numbers);
+
+Console.WriteLine(String.Join(" ", numbers));
