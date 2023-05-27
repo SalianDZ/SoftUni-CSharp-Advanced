f924a465d3708d7ed0e8961e13895ffa17af85a5
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-27 17:18:39 +0300

    07. Predicate For Names

---
+++ Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,23 @@
+﻿int length = int.Parse(Console.ReadLine());
+List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+Predicate<string> match = name => name.Length <= length;
+Func<List<string>, Predicate<string>, List<string>> filterNames = (names, match) =>
+{
+    List<string> result = new();
+
+    foreach (var name in names)
+    {
+        if (match(name))
+        {
+            result.Add(name);
+        }
+    }
+
+    return result;
+};
+
+names = filterNames(names, match);
+
+Action<List<string>> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+printNames(names);
