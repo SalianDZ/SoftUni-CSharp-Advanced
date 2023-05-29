886ca08d2745a01423632e602b8da3693afc3d00
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-29 20:05:03 +0300

    Defining Classes - Lab

---
+++ Defining Classes - Lab/02. Car Extension/Program.cs
@@ -0,0 +1,12 @@
+﻿using _02._Car_Extension;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+        }
+    }
+}
