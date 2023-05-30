4db082cd290630e9aee0a503515f31b19ae1ec6d
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-30 13:09:02 +0300

    03. Oldest Family Member

---
+++ Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+		private List<Person> familyMembers;
+
+        public Family()
+        {
+            familyMembers = new List<Person>();
+        }
+
+
+        public List<Person> People
+		{
+			get { return familyMembers; }
+			set { familyMembers = value; }
+		}
+
+		public void AddMember(Person member)
+		{
+            familyMembers.Add(member);
+		}
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = familyMembers.OrderByDescending(p => p.Age).FirstOrDefault();
+
+            return oldestPerson;
+        }
+
+
+    }
+}
---
+++ Defining Classes - Exercise/03. Oldest Family Member/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Family family = new();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                Person member = new(name, age);
+                family.AddMember(member);
+            }
+
+            Person oldestFamilyMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestFamilyMember.Name} {oldestFamilyMember.Age}");
+        }
+    }
+}
