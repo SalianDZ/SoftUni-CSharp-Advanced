3f4cdaab8ad15310ca80090a1a7923d2d91faaeb
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-05-30 14:10:41 +0300

    04. Opinion Poll

---
+++ Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List <Person> members = new();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                Person member = new(name, age);
+                members.Add(member);
+            }
+
+            //Person oldestFamilyMember = family.GetOldestMember();
+            //Console.WriteLine($"{oldestFamilyMember.Name} {oldestFamilyMember.Age}");
+            members = members.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            foreach (var person in members)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
