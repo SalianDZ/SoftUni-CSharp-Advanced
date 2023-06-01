88e3754427ae48288cb9592dc0a3fb937fd9635d
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-01 22:47:20 +0300

    Exam Exercises

---
+++ CSharp-Advanced-Exams/C# Advanced Exam - 18 February 2023/03. SoftUni Kindergarten/Kindergarten.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Child> Registry { get; set; }
+
+        public int ChildrenCount { get { return Registry.Count; } }
+
+
+        public bool AddChild(Child child)
+        {
+            if (Registry.Count < Capacity)
+            {
+                Registry.Add(child);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveChild(string childFullName)
+        {
+            string[] fullName = childFullName.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string firstName = fullName[0];
+            string lastName = fullName[1];
+            if (Registry.Any(c => c.FirstName == firstName && c.LastName == lastName))
+            {
+                int index = Registry.FindIndex(c => c.FirstName == firstName && c.LastName == lastName);
+                Registry.RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;  
+            }
+        }
+
+        public Child GetChild(string childFullName)
+        {
+            string[] fullName = childFullName.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string firstName = fullName[0];
+            string lastName = fullName[1];
+            if (Registry.Any(c => c.FirstName == firstName && c.LastName == lastName))
+            {
+                Child neededChild = Registry
+                    .Where(c => c.FirstName == firstName && c.LastName == lastName)
+                    .Select(c => c).Select(c => c).FirstOrDefault();
+                return neededChild;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string RegistryReport()
+        {
+            StringBuilder result = new();
+            result.AppendLine($"Registered children in {Name}:");
+            foreach (var child in Registry.OrderByDescending(c => c.Age).ThenBy(c => c.LastName).ThenBy(c => c.FirstName))
+            {
+                result.AppendLine(child.ToString());
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-Advanced-Exams/C# Advanced Retake Exam - 12 April 2023/03. Clothes Magazine/Magazine.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            Clothes = new List<Cloth>();
+        }
+
+        public string Type { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Cloth> Clothes { get; set; }
+
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (Clothes.Count < Capacity)
+            {
+                Clothes.Add(cloth);
+            }
+        }
+
+        public bool RemoveCloth(string color)
+        {
+            if (Clothes.Any(x => x.Color == color))
+            {
+                int indexOfWantedCloth = Clothes.FindIndex(x => x.Color == color);
+                Clothes.RemoveAt(indexOfWantedCloth);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Cloth GetSmallestCloth()
+        {
+            List<Cloth> orderedClothes = Clothes;
+            orderedClothes = orderedClothes.OrderByDescending(x => x.Size).ToList();
+            Cloth wantedCloth = orderedClothes[orderedClothes.Count - 1];
+            return wantedCloth;
+        }
+
+        public Cloth GetCloth(string color)
+        {
+            int indexOfWantedCloth = Clothes.FindIndex(x => x.Color == color);
+            return Clothes[indexOfWantedCloth];
+        }
+
+        public int GetClothCount()
+        {
+            return Clothes.Count;
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new();
+            result.AppendLine($"{Type} magazine contains:");
+            foreach (var cloth in Clothes.OrderBy(x => x.Size))
+            {
+                result.AppendLine($"{cloth}");
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
