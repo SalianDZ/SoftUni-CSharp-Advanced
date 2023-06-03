7bde99291092eb03818c8b01946217c479dab4bf
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-03 23:50:29 +0300

    Update

--- CSharp-Advanced-Exams/C# Advanced Exam - 18 February 2023/02. Blind Man's Buff/Program.cs
+++ CSharp-Advanced-Exams/C# Advanced Exam - 18 February 2023/02. Blind Man's Buff/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 string[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 int rows = int.Parse(dimensions[0]);
@@ -10,6 +12,8 @@ int manPositionCol = 0;
 for (int row = 0; row < rows; row++)
 {
     string data = Console.ReadLine();
+    data = data.Replace(" ", "");
+
 
     for (int col = 0; col < cols; col++)
     {
@@ -35,18 +39,142 @@ while (true)
     }
     else if (command == "up")
     {
-        
+        if (IndexChecker(manPositionRow, manPositionCol)
+            && IndexChecker(manPositionRow - 1, manPositionCol))
+        {
+            if (map[manPositionRow - 1, manPositionCol] == 'O')
+            {
+                continue;
+            }
+            else if (map[manPositionRow - 1, manPositionCol] == 'P')
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionRow--;
+                touchedManCounter++;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
+            else
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionRow--;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
+
+            if (touchedManCounter == 3)
+            {
+                break;
+            }
+        }
     }
     else if (command == "right")
     {
+        if (IndexChecker(manPositionRow, manPositionCol)
+            && IndexChecker(manPositionRow, manPositionCol + 1))
+        {
+            if (map[manPositionRow, manPositionCol + 1] == 'O')
+            {
+                continue;
+            }
+            else if (map[manPositionRow, manPositionCol + 1] == 'P')
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionCol++;
+                touchedManCounter++;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
+            else
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionCol++;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
 
+            if (touchedManCounter == 3)
+            {
+                break;
+            }
+        }
     }
     else if (command == "down")
     {
+        if (IndexChecker(manPositionRow, manPositionCol)
+            && IndexChecker(manPositionRow + 1, manPositionCol))
+        {
+            if (map[manPositionRow + 1, manPositionCol] == 'O')
+            {
+                continue;
+            }
+            else if (map[manPositionRow + 1, manPositionCol] == 'P')
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionRow++;
+                touchedManCounter++;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
+            else
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionRow++;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
 
+            if (touchedManCounter == 3)
+            {
+                break;
+            }
+        }
     }
     else if (command == "left")
     {
+        if (IndexChecker(manPositionRow, manPositionCol)
+            && IndexChecker(manPositionRow, manPositionCol - 1))
+        {
+            if (map[manPositionRow, manPositionCol - 1] == 'O')
+            {
+                continue;
+            }
+            else if (map[manPositionRow, manPositionCol - 1] == 'P')
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionCol--;
+                touchedManCounter++;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
+            else
+            {
+                movesCounter++;
+                map[manPositionRow, manPositionCol] = '-';
+                manPositionCol--;
+                map[manPositionRow, manPositionCol] = 'B';
+            }
 
+            if (touchedManCounter == 3)
+            {
+                break;
+            }
+        }
+    }
+}
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {touchedManCounter} Moves made: {movesCounter}");
+
+
+bool IndexChecker (int rowIndex, int colIndex)
+{
+    if (rowIndex >= 0 && rowIndex < map.GetLength(0)
+        && colIndex >= 0 && colIndex < map.GetLength(1))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
