15ee7a08b3d3d1fbb236b45bb12b206de6c329c9
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-04 17:17:24 +0300

    Generics - Lab

---
+++ Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new();
+        }
+
+        public int Count { get { return list.Count; } }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            T removedElement = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return removedElement;
+        }
+    }
+}
