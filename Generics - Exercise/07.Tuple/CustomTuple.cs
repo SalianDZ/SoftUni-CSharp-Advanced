2ca144fc531988bd99a9f8c414953b7cd04438e8
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-05 18:47:28 +0300

    07.Tuple

---
+++ Generics - Exercise/07.Tuple/CustomTuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple
+{
+    public class CustomTuple<T1, T2>
+    {
+        public CustomTuple(T1 firstItem, T2 secondItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+        }
+
+        public T1 FirstItem { get; private set; }
+
+        public T2 SecondItem { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem}";
+        }
+    }
+}
---
+++ Generics - Exercise/07.Tuple/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tuple;
+
+string[] personTokens = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] drinkTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] numberTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+CustomTuple<string, string> personTuple= new($"{personTokens[0]} {personTokens[1]}", personTokens[2]);
+
+CustomTuple<string, int> drinkTuple = new(drinkTokens[0], int.Parse(drinkTokens[1]));
+
+CustomTuple<int, double> numberTuple = new(int.Parse(numberTokens[0]), double.Parse(numberTokens[1]));
+
+Console.WriteLine(personTuple);
+Console.WriteLine(drinkTuple);
+Console.WriteLine(numberTuple);
