f5cf04928420632b237dc3d3b57d21be9b25669c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-05 15:50:53 +0300

    BoxOfInteger

---
+++ Generics - Exercise/BoxOfInteger/Program.cs
@@ -0,0 +1,14 @@
+﻿using BoxOfInteger;
+
+Box<int> box = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+
+Console.WriteLine(box.ToString());
