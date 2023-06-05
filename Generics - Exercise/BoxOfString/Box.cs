58fba0c94ac99b9caf1cc8197cf3a6d1514fdbb0
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-05 15:46:42 +0300

    Generics - Exercise

---
+++ Generics - Exercise/BoxOfString/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Generics - Exercise/BoxOfString/Program.cs
@@ -0,0 +1,14 @@
+﻿using BoxOfString;
+
+Box<string> box = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+
+    box.Add(item);
+}
+
+Console.WriteLine(box.ToString());
