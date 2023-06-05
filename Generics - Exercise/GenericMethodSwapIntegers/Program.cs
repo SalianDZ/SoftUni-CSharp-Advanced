9af79b1d25504feb8524465ee5ab3fb3bdd64d46
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-05 16:23:39 +0300

    Generics - Exercise

---
+++ Generics - Exercise/GenericMethodSwapIntegers/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericMethodSwapIntegers;
+
+Box<int> box = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+int[] indexes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+box.Swap(indexes[0], indexes[1]);
+Console.WriteLine(box.ToString());
