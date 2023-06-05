9355660349a5571297cba75648e1d5dc3a842523
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-05 17:25:06 +0300

    Generics - Exercises

---
+++ Generics - Exercise/06.GenericMethodCountDoubles/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericMethodCountDoubles;
+
+Box<double> box = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    double item = double.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+
+double elementToCompare = double.Parse(Console.ReadLine());
+Console.WriteLine(box.CompareTo(elementToCompare));
