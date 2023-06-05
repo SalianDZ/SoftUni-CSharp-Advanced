115e76597dd3b2f0b0660a2b250e9e94dd296aa6
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-05 19:15:04 +0300

    08.Threeuple

---
+++ Generics - Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,18 @@
+﻿using Threeuple;
+
+string[] personTokens = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] drinkTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] bankTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+CustomThreeuple<string, string, string> personThreeuple = new($"{personTokens[0]} {personTokens[1]}", personTokens[2], string.Join(" ", personTokens[3..]));
+
+CustomThreeuple<string, int, bool> drinkThreeuple = new(drinkTokens[0], int.Parse(drinkTokens[1]), drinkTokens[2] == "drunk");
+
+CustomThreeuple<string, double, string> numberThreeuple = new(bankTokens[0], double.Parse(bankTokens[1]), bankTokens[2]);
+
+Console.WriteLine(personThreeuple);
+Console.WriteLine(drinkThreeuple);
+Console.WriteLine(numberThreeuple);
---
+++ Generics - Exercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threeuple
+{
+    public class CustomThreeuple<T1, T2, T3>
+    {
+        public CustomThreeuple(T1 firstItem, T2 secondItem, T3 thirdItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+            ThirdItem = thirdItem;
+        }
+
+        public T1 FirstItem { get; private set; }
+
+        public T2 SecondItem { get; private set; }
+
+        public T3 ThirdItem { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem} -> {ThirdItem}";
+        }
+    }
+}
