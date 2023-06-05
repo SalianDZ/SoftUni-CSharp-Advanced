3c791d28f7cb98618451658500c54bdbf592945f
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-05 17:23:21 +0300

    Generics - Exercises

---
+++ Generics - Exercise/05.enericMethodCountStrings/Box.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethodCountStrings
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            (items[secondIndex], items[firstIndex]) = (items[firstIndex], items[secondIndex]);
+        }
+
+        public int CompareTo(T elementToCompare)
+        {
+            int count = 0;
+            foreach (var item in items)
+            {
+                if (item.CompareTo(elementToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Generics - Exercise/05.enericMethodCountStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericMethodCountStrings;
+
+Box<string> box = new();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+
+    box.Add(item);
+}
+
+string elementToCompare = Console.ReadLine();
+Console.WriteLine(box.CompareTo(elementToCompare));
