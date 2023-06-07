85ded1fc6bd178b67394d7a8a888a8166b9b01e0
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-07 22:56:30 +0300

    03. Computer Architecture

---
+++ CSharp-Advanced-Exams/C# Advanced Exam - 22 October 2022/03.Computer Architecture/Computer.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new();
+        }
+
+        public string Model { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<CPU> Multiprocessor { get; private set; }
+
+        public int Count { get { return Multiprocessor.Count; } }
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            if (Multiprocessor.Any(c => c.Brand == brand))
+            {
+                CPU cpu = Multiprocessor.Where(x => x.Brand == brand).FirstOrDefault();
+                Multiprocessor.Remove(cpu);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public CPU MostPowerful()
+        {
+            return Multiprocessor.OrderByDescending(c => c.Frequency).FirstOrDefault();
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            if (true)
+            {
+                CPU cpu = Multiprocessor.Where(x => x.Brand == brand).FirstOrDefault();
+                return cpu;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new();
+            result.AppendLine($"CPUs in the Computer {Model}:");
+            foreach (var cpu in Multiprocessor)
+            {
+                result.AppendLine(cpu.ToString());
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
