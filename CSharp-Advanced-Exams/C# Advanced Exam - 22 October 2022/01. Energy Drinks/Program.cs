48aff8553b4d7003bf68a2b0f26dd585995e87db
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-07 17:55:33 +0300

    01. Energy Drinks

---
+++ CSharp-Advanced-Exams/C# Advanced Exam - 22 October 2022/01. Energy Drinks/Program.cs
@@ -0,0 +1,44 @@
+﻿Stack<int> caffeineMilligrams = new(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+Queue<int> energyDrinks = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+int currentAmountOfCaffeine = 0;
+int allowedCaffeine = 300;
+
+while (currentAmountOfCaffeine < 300 && caffeineMilligrams.Any() && energyDrinks.Any())
+{
+    int currentCaffeineMilligrams = caffeineMilligrams.Peek();
+    int currentenergyDrink = energyDrinks.Peek();
+
+    if (currentCaffeineMilligrams * currentenergyDrink > allowedCaffeine)
+    {
+        caffeineMilligrams.Pop();
+        energyDrinks.Dequeue();
+        energyDrinks.Enqueue(currentenergyDrink);
+        currentAmountOfCaffeine -= 30;
+        if (currentAmountOfCaffeine < 0)
+        {
+            currentAmountOfCaffeine = 0;
+        }
+        allowedCaffeine += 30;
+        if (allowedCaffeine > 300)
+        {
+            allowedCaffeine = 300;
+        }
+    }
+    else
+    {
+        currentAmountOfCaffeine += currentCaffeineMilligrams * currentenergyDrink;
+        allowedCaffeine -= currentCaffeineMilligrams * currentenergyDrink;
+        caffeineMilligrams.Pop();
+        energyDrinks.Dequeue();
+    }
+}
+
+if (energyDrinks.Any())
+{
+    Console.WriteLine($"Drinks left: {string.Join(", ", energyDrinks)}");
+}
+else
+{
+    Console.WriteLine($"At least Stamat wasn't exceeding the maximum caffeine.");
+}
+Console.WriteLine($"Stamat is going to sleep with {currentAmountOfCaffeine} mg caffeine.");
